9ab75f90a76de1a6c4d2000145c7a97ae741d9d4
Funminiyi-debug <Funminiyianwo@gmail.com>
2020-08-08 19:21:49 +0100

    hi

--- server.js
+++ server.js
@@ -6,14 +6,14 @@ const dotenv = require("dotenv");
 dotenv.config({ path: "./config/config.env" });
 const mainjs = require("./main");
 
-// const app = express();
-// if (process.env.NODE_ENV === "production") {
-//   app.use(express.static("client/build"));
+const app = express();
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
 
-//   app.get("*", (req, res) => {
-//     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-//   });
-// }
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 const server = http.createServer(app);
 const io = socketio(server);
